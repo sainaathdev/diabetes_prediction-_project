1bbc65ed2c05c2dc7b2c004bb39743cbf70c6c71
sainaathdev <133358924+sainaathdev@users.noreply.github.com>
2025-07-03 12:22:11 +0530

    Add build.sh script

---
+++ diabetes_project/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files (no user input, suppress warnings)
+python manage.py collectstatic --noinput
+
+# (Optional) Run migrations
+# python manage.py migrate
